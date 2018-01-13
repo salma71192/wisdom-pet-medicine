2d6cb215002d50cce2c419f2b421b00beb7d60f3
Salma Mohammed <sm.71175@gmail.com>
2018-01-13 20:48:22 +0200

    Refactor: Delete Appointments

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,52 +1,28 @@
 import React, { Component } from 'react';
-import Data from './data.json';
 
 class Appointment extends Component {
-  state = {
-    data: []
-  }
-
-  // Load data from data.json file
-  componentDidMount() {
-    this.setState({
-      data: Data
-    });
-  }
-
   handleDelete = (e) => {
-    var allItems = this.state.data;
-    allItems.splice(e.target.value, 1);
-    console.log(e.target.value);
-    console.log(allItems);
-    this.setState({
-      data: allItems
-    });
-  };
+    this.props.onDelete(this.props.singleItem);
+  }
 
   render() {
-    var appItems = this.state.data;
-    var renderAppItems = appItems.map((item, index) => {
-          return(
-            <li className="pet-item media" key={index}>
-              <div className="pull-right">
-                <button className="pet-delete btn btn-xs btn-danger" onClick={this.handleDelete}>
-                  <span className="glyphicon glyphicon-remove"></span>
-                </button>
-              </div>
-              <div className="pet-head">
-                <span className="pet-name">{this.state.data[index].petName}</span>
-                <span className="apt-date pull-right">{this.state.data[index].aptDate}</span>
-              </div>
-              <div className="owner-name">
-                <span className="label-item">Owner: {this.state.data[index].ownerName}</span>
-              </div>
-              <div className="apt-notes">{this.state.data[index].aptNotes}</div>
-            </li>
-          )
-        });
-
     return (
-      <ul className="item-list media-list">{renderAppItems}</ul>
+      <li className="pet-item media">
+        <div className="pull-right">
+          <button className="pet-delete btn btn-xs btn-danger" onClick={this.handleDelete}>
+            <span className="glyphicon glyphicon-remove"></span>
+          </button>
+        </div>
+        <div className="pet-head">
+          <span className="pet-name">{this.props.singleItem.petName}</span>
+          <span className="apt-date pull-right">{this.props.singleItem.aptDate}</span>
+        </div>
+        <div className="owner-name">
+          <span className="label-item">Owner: {this.props.singleItem.ownerName}</span>
+        </div>
+        <div className="apt-notes">{this.props.singleItem.aptNotes}</div>
+      </li>
+
     ) // return
   } // render
 } // Appointment class
--- src/MainInterface.js
+++ src/MainInterface.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import Appointment from './Appointment';
+import Data from './data.json';
+import _ from 'lodash';
+
 
 class MainInterface extends Component {
   state = {
+    data: []
+  }
 
+  // Load data from data.json file
+  componentDidMount() {
+    this.setState({
+      data: Data
+    });
+  }
+
+  deleteAppointment(item) {
+    var appsItems = this.state.data;
+    var currentApps = _.without(appsItems, item);
+    this.setState({
+      data: currentApps
+    });
   }
 
   render() {
+    var appItems = this.state.data;
+    var renderAppItems = appItems.map((item, index) => {
+      return (
+              <Appointment
+              key={index}
+              singleItem={item}
+              onDelete={(e) => this.deleteAppointment(e)} />
+            )
+        });
+
     return (
       <div className="interface">
-        <Appointment />
+        <ul className="item-list media-list">
+        {renderAppItems}
+        </ul>
       </div>
     )
   }
